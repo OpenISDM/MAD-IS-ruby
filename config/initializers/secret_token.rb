0d8b245e0cda629e2ccf58c833a6883b944e96b2
Andrew Lai <lai61616@gmail.com>
2012-10-09 09:48:20 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MADIs::Application.config.secret_token = 'e7b8ee0d093e9787340bdf6ebc67c9bd6f6c3a2d3bdb9f85832283e98bc2459caef22e0642309b1310344645f7b74e9a581ced6e83ed18602863e252ed475f3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MADIs::Application.config.session_store :cookie_store, key: '_MAD_IS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MADIs::Application.config.session_store :active_record_store
